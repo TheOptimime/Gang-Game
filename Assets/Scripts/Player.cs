ab24a7acb3e4d78075d2d6290fad1d8ead106c89
Akebalan Etzioni <akebalan123@gmail.com>
2019-04-19 23:07:13 -0400

    Movement and Charcter Placeholders Created

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        CharacterController cc;
+
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Horizontal")!=0)
+        {
+            cc.Move(Vector3.right * Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            cc.Move(Vector3.forward * Input.GetAxis("Vertical"));
+        }
+
+    }
+}
