73c565a091eeab57215272d1bb7538b2c373f059
dejion <49814002+dejion@users.noreply.github.com>
2019-04-20 18:07:05 -0400

    updated scripts

---
+++ Assets/Scripts/BattleArenaTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleArenaTrigger : MonoBehaviour{
+    public GameObject Trigger01;
+    public GameObject Trigger02;
+    public GameObject Trigger03;
+    public GameObject Trigger04;
+    public GameObject Trigger05;
+    public GameObject Trigger06;
+    public GameObject Trigger07;
+    public GameObject Trigger08;
+    public GameObject Trigger09;
+    public GameObject Trigger10;
+    public GameObject Trigger11;
+    //These are the individual names of each trigger, in decending order from left to right. 
+
+    public void OnTriggerEnter(Collider other)
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
