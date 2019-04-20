939d8cb9b5f4257f49c7755934de922e8c2aded8
Akebalan Etzioni <akebalan123@gmail.com>
2019-04-20 13:22:46 -0400

    Battle Scene movement Updated

--- Assets/Scripts/PlayerBattle.cs
+++ Assets/Scripts/PlayerBattle.cs
@@ -4,30 +4,98 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerBattle : MonoBehaviour
+
+
 {
     // Start is called before the first frame update
     [Range(1,5)]
     public float speed;
         CharacterController cc;
 
+    Animator anim;
+
+    public bool _Movement;
+
 
     void Start()
     {
         cc = GetComponent<CharacterController>();
+        _Movement = false;
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetAxis("Horizontal")!=0)
+        //Vector3 moveposition = new Vector3();
+
+        //moveposition.x = (Input.GetAxis("Horizontal"));
+
+        //moveposition.y = (Input.GetAxis("Vertical"));
+
+
+        if (Input.GetAxis("Horizontal") > 0)
         {
             cc.Move(Vector3.right * Input.GetAxis("Horizontal") * speed);
+
+            anim.SetBool("isMovingRight", true);
+
+            _Movement = true;
         }
+        else
+        {
+            anim.SetBool("isMovingRight", false);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            cc.Move(Vector3.right * Input.GetAxis("Horizontal") * speed);
+
+            anim.SetBool("isMovingLeft", true);
+
+            _Movement = true;
+        }
+        else
+        {
+            anim.SetBool("isMovingLeft", false);
+        }
+
+
+
+
+
+
+
+
+
 
         if (Input.GetAxis("Vertical")!= 0)
         {
             cc.Move(Vector3.up * Input.GetAxis("Vertical") * speed);
+            _Movement = true;
+        }
+        else
+        {
+            _Movement = false;
+        }
+
+        Debug.Log(Input.GetAxis("Horizontal"));
+        //cc.Move(moveposition * Time.deltaTime * speed);
+    }
+
+    public void WalkAnimation()
+    {
+        if(_Movement == true)
+        {
+       
+        }
+
+        if(_Movement == false)
+        {
+            
         }
     }
+
 }
