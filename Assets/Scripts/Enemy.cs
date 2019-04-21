9f511d556cc624e74e03430d223a1838b815f9f0
Akebalan Etzioni <akebalan123@gmail.com>
2019-04-20 20:28:34 -0400

    Update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public int health, maxHealth;
+
+    bool isDamaged;
+    
+
+    public float bufferTime;
+    public float bufferMaxTime;
+
+
     void Start()
     {
+        health = maxHealth;
+
         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (!isDamaged)
+        {
+
+            if (isDamaged)
+            {
+                isDamaged = false;
+
+                bufferTime = bufferMaxTime;
+            }
+        }
+
         
     }
 }
