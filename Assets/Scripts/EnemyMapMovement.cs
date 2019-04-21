faa81ff4d899775c3e023a3c7e00dbfee77fec9a
dejion <49814002+dejion@users.noreply.github.com>
2019-04-21 01:15:54 -0400

    updated trigger system

--- Assets/Scripts/EnemyMapMovement.cs
+++ Assets/Scripts/EnemyMapMovement.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMapMovement : MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    public class EnemyPlayer
+    {
+
+    }
+    public BattleArenaTrigger bat;
+    public float Speed = 1;
+    public float Target; 
+    public int greenPoints;
+    public int redPoints;
+    private int target;
+    public CharacterController cc;
+
     void Start()
     {
-        
+        bat = FindObjectOfType<BattleArenaTrigger>();
+        cc = gameObject.AddComponent<CharacterController>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //cc.Move(Vector3.MoveTowards(transform.position, bat.triggers[0].transform.position, 2f) * Time.deltaTime * 0.1f * Speed);
+        //transform.Translate(Vector3.MoveTowards(transform.position, bat.triggers[1].transform.position, 0.001f) * Time.deltaTime * 0.1f);
+
+        if(Vector3.Distance(transform.position, bat.triggers[0].transform.position) > 0)
+        {
+            cc.Move(Vector3.MoveTowards(transform.position, bat.triggers[0].transform.position, 2f) * Time.deltaTime * 0.08f * -Speed);
+        }
+        else if(Vector3.Distance(transform.position, bat.triggers[0].transform.position) < 0)
+        {
+            cc.Move(Vector3.MoveTowards(transform.position, bat.triggers[0].transform.position, 2f) * Time.deltaTime * 0.1f * Speed);
+        }
+
+        Vector3 movePos = new Vector3();
+
+
+        movePos.x = -(transform.position.x - bat.triggers[target].transform.position.x);
+        //movePos.y = transform.position.y - bat.triggers[target].transform.position.y;
+        movePos.z = -(transform.position.z - bat.triggers[target].transform.position.z);
+
+
+
+        cc.Move(movePos * Speed* Time.deltaTime);
+
+        print(movePos);
+    
     }
 }
--- Assets/Scripts/TriggerCollision.cs
+++ Assets/Scripts/TriggerCollision.cs
@@ -6,15 +6,20 @@ using UnityEngine.SceneManagement;
 public class TriggerCollision : MonoBehaviour
 
 {
-
+    public enum ZoneColor
+    {
+        Red,
+        Green,
+        Blue,
+        Nuetral,
+    }
+    public ZoneColor zoneColor;
     public int sceneNumber;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
