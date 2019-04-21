02e64bfe30def248de7286010906c433ad8ec7f1
dejion <49814002+dejion@users.noreply.github.com>
2019-04-20 20:39:33 -0400

    Update BattleArenaTrigger.cs

--- Assets/Scripts/BattleArenaTrigger.cs
+++ Assets/Scripts/BattleArenaTrigger.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 
 public class BattleArenaTrigger : MonoBehaviour{
+
+    public GameObject[] triggers;
+    public TriggerCollision[] triggerCollisions;
+    /*
     public GameObject Trigger01;
     public GameObject Trigger02;
     public GameObject Trigger03;
@@ -28,12 +32,22 @@ public class BattleArenaTrigger : MonoBehaviour{
     public TriggerCollision trigger9;
     public TriggerCollision trigger10;
     public TriggerCollision trigger11;
+    */
 
     public int sceneNumber;
     //These are the individual names of each trigger, in decending order from left to right. 
      void Start()
     {
-        
+        /*
+        foreach(GameObject GO in triggers)
+        {
+            
+        }
+
+        for(int i = 0; i < 12; i++)
+        {
+            triggerCollisions[i].sceneNumber = i;
+        }*/
     }
 
 
