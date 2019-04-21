2b296c654cce75ac8bb39e7370985658c391d6fa
Icaru <esalley412@gmail.com>
2019-04-21 13:07:28 -0400

    Enemy movement update
    The enemy now plays it's walking animation and attack animation when close to the player. the enemy also has the ability to flip towards the player when they move left or right of it.

---
+++ Assets/Scripts/Enemy_movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Enemy_movement : MonoBehaviour {      public Transform target;     public float speed;     Animator animation;     int direction;     // Start is called before the first frame update     void Start()     {         direction = 1;         animation = GetComponent<Animator>();          target = FindObjectOfType<Player>().transform;     }     // Update is called once per frame     void Update()     {
+            Vector3 distance = transform.position - target.position;
+            Vector3 movePos = new Vector3();
+
+            if (distance.x > 0)
+            {
+                animation.SetBool("Move", true);
+                movePos.x = speed * -1; 
+            }
+            else if (distance.x < 0)
+            {
+                animation.SetBool("Move", true);
+                movePos.x = speed; 
+            }
+            if (distance.y > 0)
+            {
+                movePos.y = speed * -1;
+                animation.SetBool("Move", true);
+            }
+            else if (distance.y < 0)
+            {
+                movePos.y = speed;
+                animation.SetBool("Move", true);
+            }
+        if (distance.x < 0)
+        {
+
+
+            if (direction < 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+                direction *= -1;
+            }
+        } 
+        if (distance.x > 0)
+        {
+
+
+            if (direction > 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+                direction *= -1;
+            }
+        }         if (distance.x < 20)         {             animation.SetBool("isAttacking",true);         }         if (distance.x > -20)         {             animation.SetBool("isAttacking", true);         }         if (distance.x > 20)         {             animation.SetBool("isAttacking", false);         }         if (distance.x < -20)         {             animation.SetBool("isAttacking", false);         }
+        transform.Translate(movePos * Time.deltaTime * speed);
+    }                
+}
