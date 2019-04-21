d2063ffbc04d3d5e9631c0e66f08dfd479598748
dejion <49814002+dejion@users.noreply.github.com>
2019-04-21 01:22:46 -0400

    new

--- Assets/Scripts/TriggerCollision.cs
+++ Assets/Scripts/TriggerCollision.cs
@@ -6,15 +6,22 @@ using UnityEngine.SceneManagement;
 public class TriggerCollision : MonoBehaviour
 
 {
-
+  public enum ZoneColor
+    {
+        Blue,
+        Green,
+        Red,
+        Nuetral,
+    }
+    public ZoneColor zoneColor;
     public int sceneNumber;
-    // Start is called before the first frame update
+  
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
